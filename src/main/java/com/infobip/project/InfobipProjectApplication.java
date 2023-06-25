7f740b5c66ce8d07ae0f63817a6f9254569641e8
Jac_Hac <jh52339@fer.hr>
2023-06-25 10:57:07 +0200

    first commit

---
+++ src/main/java/com/infobip/project/InfobipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.infobip.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfobipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfobipProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infobip/project/InfobipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infobip.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfobipProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
